ede26fa9ea927eb75707524ec805fcdab6615aef
Prepe <37080825+Prepe@users.noreply.github.com>
2025-04-05 17:16:46 +0200

    Player Movement Improvement

--- Player/player.gd
+++ Player/player.gd
@@ -1,21 +1,36 @@
 extends CharacterBody2D
 
-var speed = 500
-var gravity = 300
+@export var speed : int = 500
+@export var gravity : int = 300
 
 func _physics_process(delta: float) -> void:	
-	var dir = Input.get_vector("Left", "Right", "Up", "Down")
-	dir = dir.normalized() * speed
+	var dir : Vector2 = Input.get_vector("Left", "Right", "Up", "Down")	
 	
-	var collides = move_and_slide()
-	if (dir != Vector2.ZERO):
-		if (collides): velocity = velocity * -0.5
-		else:
-			velocity += dir * delta
-			if abs(velocity.x) > abs(dir.x): velocity.x = dir.x
-			if abs(velocity.y) > abs(dir.y): velocity.y = dir.y
-	elif (collides):
-		velocity = Vector2.ZERO
-	elif (velocity.y < gravity): 
-		velocity.x = 0
-		velocity.y = velocity.y + gravity * delta
+	if (dir.x != 0):
+		dir.x *= speed * delta * 2
+		if((dir.x < 0 && velocity.x > 0) || (dir.x > 0 && velocity.x < 0)): dir.x += (velocity.x * 0.75)
+		else: dir.x += velocity.x
+		if (dir.x > speed): dir.x = speed
+		elif (dir.x < -speed): dir.x = -speed
+	elif (abs(velocity.x) > 10): dir.x = velocity.x * 0.9
+	else: dir.x = 0
+	
+	if (dir.y != 0):
+		dir.y *= speed * delta * 2
+		if((dir.y < 0 && velocity.y > 0) || (dir.y > 0 && velocity.y < 0)): dir.y += (velocity.y * 0.75)
+		else: dir.y += velocity.y
+		if (dir.y > speed): dir.y = speed
+		elif (dir.y < -speed): dir.y = -speed
+	elif (abs(velocity.y) > 10): dir.y = velocity.y * 0.9
+	else: dir.y = 0 
+	
+	velocity = dir
+	move_and_slide()
+#	if(move_and_slide()):
+#		var collition
+#		for i in get_slide_collision_count():
+#			dir = position - get_slide_collision(i).get_position() * speed * 0.3
+#			velocity += dir
+#		
+#		velocity.x = clamp(velocity.x, -speed, speed)
+#		velocity.y = clamp(velocity.y, -speed, speed)
