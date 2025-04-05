db141d4b3cd7b78e9b81ec8ed1643fcbcd4f6d9e
Prepe <37080825+Prepe@users.noreply.github.com>
2025-04-05 14:39:36 +0200

    Tilemap initialized
    Player movement extendet

--- Player/player.gd
+++ Player/player.gd
@@ -1,12 +1,21 @@
 extends CharacterBody2D
 
-var speed = 200
+var speed = 500
+var gravity = 300
 
-func _physics_process(_delta: float) -> void:	
+func _physics_process(delta: float) -> void:	
 	var dir = Input.get_vector("Left", "Right", "Up", "Down")
 	dir = dir.normalized() * speed
 	
-	velocity = dir
-	if (move_and_slide()):
-		dir = dir * -0.5
-		velocity = dir
+	var collides = move_and_slide()
+	if (dir != Vector2.ZERO):
+		if (collides): velocity = velocity * -0.5
+		else:
+			velocity += dir * delta
+			if abs(velocity.x) > abs(dir.x): velocity.x = dir.x
+			if abs(velocity.y) > abs(dir.y): velocity.y = dir.y
+	elif (collides):
+		velocity = Vector2.ZERO
+	elif (velocity.y < gravity): 
+		velocity.x = 0
+		velocity.y = velocity.y + gravity * delta
