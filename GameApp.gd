ef7b2cef38542a9b267cbf0c8c2b67fd8dd0c1e7
Prepe <37080825+Prepe@users.noreply.github.com>
2025-04-07 01:02:53 +0200

    Updated some Project settings
    Fixing NaN Pos for player
    resorting the Draw order for the TestScene

--- GameApp.gd
+++ GameApp.gd
@@ -7,20 +7,20 @@ extends Node2D
 var world: WorldFactory
 
 func _ready() -> void:
-    # build the world
-    world = WorldFactory.new(ground)
-    
-    # adapt the player zoom level
-    if mainCam != null:
-        mainCam.zoom = Vector2(0.25, 0.25)
-    
-    # adapt the player position
-    if player != null:
-        player.collides.connect(_on_collision_detected)
-        var center: int = WorldFactory.SECTION_ROWS * WorldFactory.SECTION_COUNT
-        var centerPoint = ground.map_to_local(Vector2i(center, center))
-        player.global_position = Vector2(centerPoint.x, player.global_position.y)
-        
+	# build the world
+	world = WorldFactory.new(ground)
+	
+	# adapt the player zoom level
+	if mainCam != null:
+		mainCam.zoom = Vector2(0.25, 0.25)
+	
+	# adapt the player position
+	if player != null:
+		player.collides.connect(_on_collision_detected)
+		var center: int = WorldFactory.SECTION_ROWS * WorldFactory.SECTION_COUNT
+		var centerPoint = ground.map_to_local(Vector2i(center, center))
+		player.global_position = Vector2(centerPoint.x, player.global_position.y)
+		
 func _on_collision_detected(collision: KinematicCollision2D) -> void:
-    var cellPosition: Vector2i = ground.get_coords_for_body_rid(collision.get_collider_rid())
-    world.drill(cellPosition)
+	var cellPosition: Vector2i = ground.get_coords_for_body_rid(collision.get_collider_rid())
+	world.drill(cellPosition)
--- Player/player.gd
+++ Player/player.gd
@@ -36,6 +36,8 @@ func _process(delta: float) -> void:
 
 func _physics_process(delta: float) -> void:
 	var dir : Vector2 = Vector2.ZERO
+	var dirMod : Vector2
+	
 	if (!menuHud.visible): 
 		dir = Input.get_vector("Left", "Right", "Up", "Down")
 		
@@ -57,8 +59,9 @@ func _physics_process(delta: float) -> void:
 		elif (abs(velocity.y) > 10): dir.y = velocity.y * 0.8
 		else: dir.y = 0 
 	
-	if (abs(dir.x) > speedLimit): dir.x = speedLimit * (dir.x / abs(dir.x))
-	if (abs(dir.y) > speedLimit): dir.y = speedLimit * (dir.y / abs(dir.y))
+	dirMod = directionMod(dir)
+	if (abs(dir.x) > speedLimit): dir.x = speedLimit * dirMod.x
+	if (abs(dir.y) > speedLimit): dir.y = speedLimit * dirMod.y
 	velocity = dir
 	var pos : Vector2 = position
 	if(move_and_slide()):
@@ -66,13 +69,22 @@ func _physics_process(delta: float) -> void:
 		if dir != Vector2.ZERO:
 			dir = dir.normalized()
 			pos = abs(pos - position) / abs(dir)
-			dir = (dir / abs(dir)) * bounceForce * -1
+			dirMod = directionMod(dir)
+			dir = dirMod * bounceForce * -1
 			if (pos.x > pos.y): velocity.y = dir.y
 			elif (pos.x < pos.y): velocity.x = dir.x
 			else: velocity = dir
 		
 		if (abs(dir.x) > speedLimit): dir.x = speedLimit * (dir.x / abs(dir.x))
 		if (abs(dir.y) > speedLimit): dir.y = speedLimit * (dir.y / abs(dir.y))
+
+func directionMod(dir : Vector2) -> Vector2:
+	if dir.x < 0: dir.x = -1
+	elif dir.x > 0: dir.x = 1
+	
+	if dir.y < 0: dir.y = -1
+	elif dir.y > 0: dir.y = 1
+	return dir
 
 func _on_menu_hud_buy_shop_selection(itemType: int) -> void:
 	match itemType:
