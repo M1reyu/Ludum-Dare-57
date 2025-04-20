359ba87d1a15f044ab903383b603a622058610bf
prepe <martin.proprentner@gmx.at>
2025-04-20 14:04:03 +0200

    Refactored Assets Scenes and Script folder structur

---
+++ Scripts/Player/ShopHud.gd
@@ -0,0 +1,172 @@
+extends Control
+
+var canClose : bool = false
+var menuIndex : int = 0
+var MenuSection1 : Node
+var MenuSection2 : Node
+var MenuSection3 : Node
+
+var MenuSection1Count : int = 0
+var MenuSection2Count : int = 0
+var MenuSection3Count : int = 0
+
+var ItemName : Label
+var ItemInfo : Label
+var ItemCost : Label
+
+const shopItem = GlobalVars.shopBuyables
+var shopCalc = GlobalVars.ShopCalc.new()
+signal buyShopSelection(itemIndex : int)
+
+var funds: int
+var hp: int
+var hpMax: int
+var fuel: int
+var fuelMax: int
+var cargo: int
+var cargoMax: int
+var speedMax: int
+var strength : int
+var bombs: int
+var miners: int
+var shielded: bool
+var scanner: bool
+var flagging: bool
+var rangeMine: bool
+
+func _ready() -> void:
+	MenuSection1 = $ShopSelector/Utilities
+	MenuSection1Count = MenuSection1.get_child_count()
+	
+	MenuSection2 = $ShopSelector/Upgrades
+	MenuSection2Count = MenuSection2.get_child_count()
+	
+	MenuSection3 = $ShopSelector/Apilities
+	MenuSection3Count = MenuSection3.get_child_count()
+	
+	ItemName = $Layout/InfoPanel/ItemName
+	ItemInfo = $Layout/InfoPanel/ItemInfo
+	ItemCost = $Layout/InfoPanel/ItemCost
+	
+	hide()
+
+func _process(_delta: float) -> void:
+	if (visible && canClose && Input.is_action_just_pressed("MenuTrigger")): 
+		canClose = false
+		hide()
+	
+	if (!visible): return
+	if (!canClose):
+		canClose = true
+		menuIndex = 1
+		highlightMenu()
+	
+	if (Input.is_action_just_pressed("Left")): 
+		menuIndex -= 1
+		if menuIndex == 0: menuIndex += MenuSection1Count
+		elif menuIndex == MenuSection1Count: menuIndex += MenuSection2Count
+		elif menuIndex == MenuSection1Count + MenuSection2Count: menuIndex += MenuSection3Count
+	elif (Input.is_action_just_pressed("Right")): 
+		if menuIndex == MenuSection1Count: menuIndex -= MenuSection1Count
+		elif menuIndex == MenuSection1Count + MenuSection2Count: menuIndex -= MenuSection2Count
+		elif menuIndex == MenuSection1Count + MenuSection2Count + MenuSection3Count: menuIndex -= MenuSection3Count
+		menuIndex += 1 
+	elif (Input.is_action_just_pressed('Up')):
+		if menuIndex <= MenuSection1Count: menuIndex += MenuSection1Count + MenuSection2Count
+		elif menuIndex <= MenuSection1Count + MenuSection2Count: menuIndex -= MenuSection1Count
+		else: menuIndex -= MenuSection2Count
+		
+		menuIndex = min(menuIndex, MenuSection1Count + MenuSection2Count + MenuSection3Count) 
+	elif (Input.is_action_just_pressed('Down')):
+		if menuIndex <= MenuSection1Count: menuIndex += MenuSection1Count
+		elif menuIndex <= MenuSection1Count + MenuSection2Count: menuIndex += MenuSection2Count
+		else: menuIndex -= (MenuSection1Count + MenuSection2Count)
+		
+		menuIndex = min(menuIndex, MenuSection1Count + MenuSection2Count + MenuSection3Count) 
+	elif (Input.is_action_just_pressed('Use')):
+		var cost = getCostForItem()
+		if (0 < cost && cost <= funds):
+			buyShopSelection.emit(menuIndex)
+	else: return
+	
+	highlightMenu()
+
+func highlightMenu():
+	var iMod = menuIndex
+	var nodeCount = MenuSection1.get_child_count()
+	var node : Node
+	for i in range(nodeCount):
+		node = MenuSection1.get_child(i)
+		if (i+1 == iMod): node.show()
+		else: node.hide()
+	
+	iMod -= nodeCount
+	
+	nodeCount = MenuSection2.get_child_count()
+	for i in range(nodeCount):
+		node = MenuSection2.get_child(i)
+		if (i+1 == iMod): node.show()
+		else: node.hide()
+	
+	iMod -= nodeCount
+	
+	nodeCount = MenuSection3.get_child_count()
+	for i in range(nodeCount):
+		node = MenuSection3.get_child(i)
+		if (i+1 == iMod): node.show()
+		else: node.hide()
+	
+	ItemName.text = GlobalVars.itemNames.get(menuIndex)
+	ItemInfo.text = GlobalVars.itemInfos.get(menuIndex)
+	var cost = getCostForItem()
+	ItemCost.text = "-" if cost < 0 else str(cost)
+	if funds < cost: ItemCost.set("theme_override_colors/font_color", Color.RED)
+	else: ItemCost.set("theme_override_colors/font_color", Color.BLACK)
+
+func getCostForItem() -> int:
+	match menuIndex:
+		shopItem.Repair:
+			return shopCalc.getCost(menuIndex, hp, hpMax, funds)
+		shopItem.Refuel:
+			return shopCalc.getCost(menuIndex, fuel, fuelMax, funds)
+		shopItem.Shield:
+			return shopCalc.getCost(menuIndex, 1 if shielded else 0)
+		shopItem.Bomb:
+			return -1 if (bombs == 10) else shopCalc.getCost(menuIndex)
+		shopItem.Miner:
+			return -1 if (miners == 10) else shopCalc.getCost(menuIndex)
+		shopItem.HealthUp:
+			return shopCalc.getCost(menuIndex, hpMax)
+		shopItem.SpeedUp:
+			return shopCalc.getCost(menuIndex, speedMax)
+		shopItem.StrengthUp:
+			return shopCalc.getCost(menuIndex, strength)
+		shopItem.TankUp:
+			return shopCalc.getCost(menuIndex, fuelMax)
+		shopItem.CargoUp:
+			return shopCalc.getCost(menuIndex, cargoMax)
+		shopItem.Scanner:
+			return shopCalc.getCost(menuIndex, 1 if scanner else 0)
+		shopItem.Flag:
+			return shopCalc.getCost(menuIndex, 1 if flagging else 0)
+		shopItem.RangeMine:
+			return shopCalc.getCost(menuIndex, 1 if rangeMine else 0)
+	
+	return shopCalc.getCost(menuIndex)
+
+func _on_player_player_stats(funds: int, hp: int, hpMax: int, fuel: int, fuelMax: int, cargo: int, cargoMax: int, speedMax: int, strength : int, bombs: int, miners: int, shielded: bool, scanner: bool, flagging: bool, rangeMine: bool) -> void:
+	self.funds = funds
+	self.hp = hp
+	self.hpMax = hpMax
+	self.fuel = fuel
+	self.fuelMax = fuelMax
+	self.cargo = cargo
+	self.cargoMax = cargoMax
+	self.speedMax = speedMax
+	self.strength = strength
+	self.bombs = bombs
+	self.miners = miners
+	self.shielded = shielded
+	self.scanner = scanner
+	self.flagging = flagging
+	self.rangeMine = rangeMine
---
+++ Scripts/Player/player.gd
@@ -0,0 +1,278 @@
+extends CharacterBody2D
+
+signal collides(collision: KinematicCollision2D)
+signal scan(coordinates: Vector2)
+signal tnt(coordinates: Vector2)
+signal activateSkill(skillType: int)
+
+@export var speed : int = 800
+@export var speedLimit : int = 1500
+@export var bounceForce : int = 1000
+#@export var gravity : int = 300
+
+@export var maxHealth : int = 2
+@export var maxTank : int = 100
+@export var maxCargo : int = 10
+
+@export_file('*.tscn') var deathScenePath: String = ''
+
+var strength : int = 2
+var inMenu : bool = false
+var canOpen : bool = false
+
+var curMoney : int = 0
+var curHealth : int = 1
+var curTank : float = 100.0
+var curCargo : int = 0
+var shielded : bool = false
+var bombCount : int = 0
+var minerCount : int = 0
+var scannerBought : bool = false
+var flaggingBought : bool = false
+var playerStrength : int
+var playerSpeed : int
+
+@onready var menuHud : Control = $Canvas/ShopHud
+@onready var playerSprite : AnimatedSprite2D = $PlayerAssets/PlayerSprite
+@onready var animations : AnimationPlayer = get_node("AnimationPlayer")
+@onready var flagmodeFlag : Sprite2D = $PlayerAssets/FlagMode
+@onready var autominerMode : Sprite2D = $PlayerAssets/AutoMinerMode
+@onready var shopPrompt : Control = $PlayerPopups/ShopPrompt
+@onready var oreGainNode : Control = $PlayerPopups/OreGains
+
+const shopItem = GlobalVars.shopBuyables
+var shopCalc = GlobalVars.ShopCalc.new()
+
+var tntScene = preload("res://Scenes/Misc/TnT.tscn")
+var oreScene = preload("res://Scenes/Player/ore_pop_up.tscn")
+
+signal playerStats(funds : int, hp : int, hpMax : int, fuel : int, fuelMax : int, cargo : int, cargoMax : int, speedMax : int, bombs : int, miners : int, shielded : bool, scanner : bool, flagging : bool, rangeMine : bool)
+
+func _ready() -> void:
+	curHealth = maxHealth
+	curTank = maxTank
+	playerStrength = strength
+	playerSpeed = speed
+	
+	GlobalVars.timePlayed = 0
+	GlobalVars.tilesMined = 0
+	GlobalVars.minesHit = 0
+	GlobalVars.oreMined = 0
+	GlobalVars.drillSpreading = false
+	
+	sendStatSignal()
+
+var scanTimeout = 0
+var tntTimeout = 0
+func _process(delta: float) -> void:	
+	GlobalVars.timePlayed += delta
+	
+	if scanTimeout > 0: scanTimeout -= delta
+	if tntTimeout > 0: tntTimeout -= delta
+	if (canOpen && !menuHud.visible && Input.is_action_just_pressed("MenuTrigger")): 
+		sendStatSignal()
+		menuHud.show()
+	
+	flagmodeFlag.visible = true if (strength == -1) else false
+	autominerMode.visible = true if (strength < -1) else false
+	
+	var dirMod = directionMod(Input.get_vector("Left", "Right", "Up", "Down"))	
+	if (dirMod == Vector2.ZERO || menuHud.visible): 
+		playerSprite.rotation_degrees = 0
+		if (playerSprite.animation != "Idle"): playerSprite.play("Idle")
+	elif (strength >= 0):
+		playerSprite.rotation = dirMod.angle() - Vector2.DOWN.angle()
+		if (playerSprite.animation == "Idle"): playerSprite.play("Drill 1")
+	
+	if (Input.is_action_just_pressed("UseTnT")):
+		if bombCount > 0 && tntTimeout <= 0:
+			tntTimeout = GlobalVars.cooldowns.get(shopItem.Bomb)
+			bombCount -= 1
+			var node : TNTNode = tntScene.instantiate()
+			add_sibling(node)
+			node.global_position = global_position
+			node.explode.connect(_on_tn_t_explode)
+			activateSkill.emit(shopItem.Bomb)
+		sendStatSignal()
+	elif (Input.is_action_just_pressed("UseMiner")):
+		if minerCount > 0:
+			if strength != 0 && strength != -1: strength *= -1
+		sendStatSignal()
+	elif (Input.is_action_just_pressed("UseScan")):
+		if scannerBought && scanTimeout <= 0:
+			scanTimeout = GlobalVars.cooldowns.get(shopItem.Scanner)
+			scan.emit(playerSprite.global_position)
+			activateSkill.emit(shopItem.Scanner)
+		sendStatSignal()
+	elif (Input.is_action_just_pressed("UseFlag")):
+		if flaggingBought:
+			if strength > 0: strength = -1
+			elif strength == -1: strength = playerStrength
+		sendStatSignal()
+
+func _physics_process(delta: float) -> void:
+	var dir : Vector2 = Vector2.ZERO
+	var dirMod : Vector2
+	
+	if (!menuHud.visible): 
+		dir = Input.get_vector("Left", "Right", "Up", "Down")
+		if (dir != Vector2.ZERO && curTank > 0): 
+			if (int(curTank - delta) < int(curTank)): sendStatSignal()
+			curTank -= delta
+			if curTank <= 0:
+				curTank = 0
+				strength = 0
+				speed = playerSpeed / 2
+			else:
+				if strength == 0: strength = playerStrength
+				speed = playerSpeed
+			
+		if (dir.x != 0):
+			dir.x *= speed * delta * 2
+			if((dir.x < 0 && velocity.x > 0) || (dir.x > 0 && velocity.x < 0)): dir.x += (velocity.x * 0.65)
+			else: dir.x += velocity.x
+			if (dir.x > speed): dir.x = speed
+			elif (dir.x < -speed): dir.x = -speed
+		elif (abs(velocity.x) > 10): dir.x = velocity.x * 0.8
+		else: dir.x = 0
+		
+		if (dir.y != 0):
+			dir.y *= speed * delta * 2
+			if((dir.y < 0 && velocity.y > 0) || (dir.y > 0 && velocity.y < 0)): dir.y += (velocity.y * 0.65)
+			else: dir.y += velocity.y
+			if (dir.y > speed): dir.y = speed
+			elif (dir.y < -speed): dir.y = -speed
+		elif (abs(velocity.y) > 10): dir.y = velocity.y * 0.8
+		else: dir.y = 0 
+	
+	dirMod = directionMod(dir)
+	if (abs(dir.x) > speedLimit): dir.x = speedLimit * dirMod.x
+	if (abs(dir.y) > speedLimit): dir.y = speedLimit * dirMod.y
+	velocity = dir
+	var pos : Vector2 = position
+	if(move_and_slide()):
+		collides.emit(get_last_slide_collision())
+		if dir != Vector2.ZERO:
+			dir = dir.normalized()
+			pos = abs(pos - position) / abs(dir)
+			dirMod = directionMod(dir)
+			dir = dirMod * bounceForce * -1
+			if (pos.x > pos.y): velocity.y = dir.y
+			elif (pos.x < pos.y): velocity.x = dir.x
+			else: velocity = dir
+		
+		if (abs(dir.x) > speedLimit): dir.x = speedLimit * (dir.x / abs(dir.x))
+		if (abs(dir.y) > speedLimit): dir.y = speedLimit * (dir.y / abs(dir.y))
+
+func sendStatSignal() -> void:
+	playerStats.emit(curMoney, curHealth, maxHealth, curTank, maxTank, curCargo, maxCargo, playerSpeed, playerStrength, bombCount, minerCount, shielded, scannerBought, flaggingBought, GlobalVars.drillSpreading)
+
+func directionMod(dir : Vector2) -> Vector2:
+	if dir.x < 0: dir.x = -1
+	elif dir.x > 0: dir.x = 1
+	
+	if dir.y < 0: dir.y = -1
+	elif dir.y > 0: dir.y = 1
+	return dir
+
+func _on_shop_hud_buy_shop_selection(itemType: int) -> void:
+	var cost = 0
+	
+	match itemType:
+		shopItem.Repair:
+			cost = shopCalc.getCost(itemType, curHealth, maxHealth, curMoney)
+			curHealth += (cost / 150)
+			if curHealth > maxHealth: curHealth = maxHealth
+		shopItem.Refuel:
+			cost = shopCalc.getCost(itemType, curTank, maxTank, curMoney)
+			curTank += cost
+			if curTank > maxTank: curTank = maxTank
+		shopItem.Shield:
+			cost = shopCalc.getCost(itemType)
+			shielded = true
+		shopItem.Bomb:
+			cost = shopCalc.getCost(itemType)
+			bombCount += 1
+		shopItem.Miner:
+			cost = shopCalc.getCost(itemType)
+			minerCount += 1
+		shopItem.HealthUp:
+			cost = shopCalc.getCost(itemType, maxHealth)
+			maxHealth += 1
+			curHealth = maxHealth
+		shopItem.SpeedUp:
+			cost = shopCalc.getCost(itemType, playerSpeed)
+			playerSpeed += 200
+		shopItem.StrengthUp:
+			cost = shopCalc.getCost(itemType, playerStrength)
+			playerStrength *= 2
+			strength = playerStrength
+		shopItem.TankUp:
+			cost = shopCalc.getCost(itemType, maxTank)
+			maxTank *= 2
+			curTank = maxTank
+		shopItem.CargoUp:
+			cost = shopCalc.getCost(itemType, maxCargo)
+			maxCargo += 10
+		shopItem.Scanner:
+			cost = shopCalc.getCost(itemType)
+			scannerBought = true
+		shopItem.Flag:
+			cost = shopCalc.getCost(itemType)
+			flaggingBought = true
+		shopItem.RangeMine:
+			cost = shopCalc.getCost(itemType)
+			GlobalVars.drillSpreading = true
+	
+	curMoney -= cost
+	AudioPlayer.play_sfx("powerUp")
+	sendStatSignal()
+
+func _on_player_tig_area_area_entered(_area: Area2D) -> void:
+	canOpen = true
+	var moneyEarned: int = curCargo * 100
+	curMoney += moneyEarned
+	curCargo = 0
+	shopPrompt.visible = true
+	
+	sendStatSignal()
+
+func _on_player_tig_area_area_exited(_area: Area2D) -> void:
+	canOpen = false
+	shopPrompt.visible = false
+	
+	sendStatSignal()
+
+func _on_explosion(coordinates: Vector2i, damage: int, radius: int) -> void:
+	#print("explosion...")
+	if position.distance_to(coordinates) > radius:
+		return
+
+	if shielded:
+		shielded = false
+	else:
+		curHealth -= damage
+	
+	if curHealth <= 0: 
+		GlobalVars.playerFunds = curMoney
+		get_tree().change_scene_to_file(deathScenePath)
+	
+	animations.play("playerDmg")
+	sendStatSignal()
+
+
+func _on_collect_valuable(value: int) -> void:
+	var oldCargo: int = curCargo
+	curCargo = min(curCargo + value, maxCargo)
+	var diffCargo = curCargo - oldCargo
+	var oreGain : Control = oreScene.instantiate()
+	oreGainNode.add_child(oreGain)
+	var label: Label = oreGain.get_child(0)
+	if diffCargo == 0: label.text = "full"
+	else: label.text = "+" + str(diffCargo)
+	
+	sendStatSignal()
+
+func _on_tn_t_explode(coordinates: Vector2i, dmg: int) -> void:
+	_on_explosion(coordinates, dmg, 550)
+	tnt.emit(coordinates)
